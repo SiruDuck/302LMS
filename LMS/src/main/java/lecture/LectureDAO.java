e355d0fdbe1cbe296af275f26c6140be8cc4c12e
kimseoine <potato32122@gmail.com>
2022-11-15 12:29:09 +0900

    dd
    dd

--- LMS/src/main/java/lecture/LectureDAO.java
+++ LMS/src/main/java/lecture/LectureDAO.java
@@ -10,59 +10,37 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class LectureDAO implements LectureService {
 	@Autowired @Qualifier("ymu") private SqlSession sql;
-
+	
 	@Override
 	public List<LectureVO> lecture_list() {
 		
 		return sql.selectList("lecture.list");
 	}
+
+	@Override
+	public int lecture_insert(LectureVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void lecture_update(LectureVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lecture_delete(String lecture_num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lecture_info(String lecture_num) {
+		// TODO Auto-generated method stub
+		
+	}
 	
-	@Override
-	public int lecture_insert(LectureVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void lecture_update(LectureVO vo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void lecture_delete(String lecture_num) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void lecture_info(String lecture_num) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int lecture_insert(LectureVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void lecture_update(LectureVO vo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void lecture_delete(String lecture_num) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void lecture_info(String lecture_num) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
--- LMS/src/main/java/lecture/LectureVO.java
+++ LMS/src/main/java/lecture/LectureVO.java
@@ -92,11 +92,9 @@ public class LectureVO {
 	public void setLecture_year(String lecture_year) {
 		this.lecture_year = lecture_year;
 	}
-
 	public int getLecture_num() {
 		return lecture_num;
 	}
-
 	public void setLecture_num(int lecture_num) {
 		this.lecture_num = lecture_num;
 	}
@@ -111,124 +109,7 @@ public class LectureVO {
 	}
 	public void setFinalex(Date finalex) {
 		this.finalex = finalex;
-
 	}
-
-	public String getTeacher_name() {
-		return teacher_name;
-	}
-
-	public void setTeacher_name(String teacher_name) {
-		this.teacher_name = teacher_name;
-	}
-
-	public String getSemester() {
-		return semester;
-	}
-
-	public void setSemester(String semester) {
-		this.semester = semester;
-	}
-
-	public String getSortation() {
-		return sortation;
-	}
-
-	public void setSortation(String sortation) {
-		this.sortation = sortation;
-	}
-
-	public String getLecture_room() {
-		return lecture_room;
-	}
-
-	public void setLecture_room(String lecture_room) {
-		this.lecture_room = lecture_room;
-	}
-
-	public String getLecture_time() {
-		return lecture_time;
-	}
-
-	public void setLecture_time(String lecture_time) {
-		this.lecture_time = lecture_time;
-	}
-
-	public String getEnrolment() {
-		return enrolment;
-	}
-
-	public void setEnrolment(String enrolment) {
-		this.enrolment = enrolment;
-	}
-
-	public String getReception_status() {
-		return reception_status;
-	}
-
-	public void setReception_status(String reception_status) {
-		this.reception_status = reception_status;
-	}
-
-	public String getCapacity() {
-		return capacity;
-	}
-
-	public void setCapacity(String capacity) {
-		this.capacity = capacity;
-	}
-
-	public String getSubjectcredit() {
-		return subjectcredit;
-	}
-
-	public void setSubjectcredit(String subjectcredit) {
-		this.subjectcredit = subjectcredit;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getBook() {
-		return book;
-	}
-
-	public void setBook(String book) {
-		this.book = book;
-	}
-
-	public Date getLecture_year() {
-		return lecture_year;
-	}
-
-	public void setLecture_year(Date lecture_year) {
-		this.lecture_year = lecture_year;
-	}
-
-	public Date getMidex() {
-		return midex;
-	}
-
-	public void setMidex(Date midex) {
-		this.midex = midex;
-	}
-
-	public Date getFinalex() {
-		return finalex;
-	}
-
-	public void setFinalex(Date finalex) {
-		this.finalex = finalex;
-	}
-
-	
-	
-	
 	
 	
 	
