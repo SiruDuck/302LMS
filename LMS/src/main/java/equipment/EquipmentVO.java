5022735b90d32c86a8bfec05bfa1b0a4bc7f3dd1
kimseoine <potato32122@gmail.com>
2022-11-17 16:05:19 +0900

    d
    d

---
+++ LMS/src/main/java/equipment/EquipmentVO.java
@@ -0,0 +1,22 @@
+package equipment;
+
+public class EquipmentVO {
+	private String equipment;
+	private int equipment_num;
+	
+	
+	public String getEquipment() {
+		return equipment;
+	}
+	public void setEquipment(String equipment) {
+		this.equipment = equipment;
+	}
+	public int getEquipment_num() {
+		return equipment_num;
+	}
+	public void setEquipment_num(int equipment_num) {
+		this.equipment_num = equipment_num;
+	}
+	
+	
+}
