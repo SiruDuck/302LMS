5be4726ad7f59eb1a78a77d82a3a4b453b868f98
kimseoine <potato32122@gmail.com>
2022-11-21 15:42:09 +0900

    ㅇ

--- LMS/src/main/java/com/yongmoon/lms/MemberController.java
+++ LMS/src/main/java/com/yongmoon/lms/MemberController.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,7 +16,6 @@ import department.DepartmentVO;
 import equipment.EquipmentDAO;
 import equipment.EquipmentVO;
 
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
